829c34a448e695755b8d50af17d5c56dddd289e1
Thijs <thijs@tyz.nl>
2023-08-28 17:30:38 +0200

    Create simple landing page for TyZ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,128 @@
-import Image from 'next/image'
-
-export default function Home():JSX.Element {
+export default function Home(): JSX.Element {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <header className="w-full flex flex-col items-center p-10">
+        <div className="flex max-w-5xl w-full">
+          <div className="w-1/2">
+            <h1 className="text-stone-800 text-5xl">TyZ</h1>
+            <h3 className="text-stone-700 text-xl">Think. Yield. Zenith.</h3>
+          </div>
+
+          <p className="w-1/2 text-right">
+            <a href="mailto:thijs@tyz.nl">thijs@tyz.nl</a>
+            <br />
+            <a href="tel:0689936821">06 899 36 821</a>
+          </p>
+        </div>
+      </header>
+
+      <div className="max-w-5xl relative flex flex-col place-items-center">
+        <h2 className="text-stone-800 text-7xl p-5">Thijs Broerse</h2>
+        <h3 className="text-stone-700 text-3xl">
+          Interim Tech Lead & Freelance Frontend Architect
+        </h3>
+
+        <p className="p-10">
+          Highly skilled Frontend expert with over 20 years of experience in web
+          development and leadership. Able to set up, develop and maintain
+          complex (frontend) systems, leading (international) development teams.
+          Worked on many (award winning) projects, specialized in scripting and
+          architecture
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+        <div className="">
+          <h2 className="mb-3 text-2xl font-semibold">Leadership</h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Lead and mentor development teams with agile methodologies
+          </p>
+        </div>
+
+        <div className="">
+          <h2 className="mb-3 text-2xl font-semibold">Architecture</h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Build complex web applications. Setup APIs.
+          </p>
+        </div>
+
+        <div className="">
+          <h2 className="mb-3 text-2xl font-semibold">Development</h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Write solid and clean code with TypeScript or plain JS. Preferred in
+            React or Angular, Vue or Vanilla JS
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+      <footer className="w-full text-white flex flex-col items-center p-10">
+        <div className="flex max-w-5xl w-full">
+          <p className="w-1/2">
+            Lingewijk 14
+            <br />
+            3831LH Leusden
+            <br />
+            The Netherlands
+            <br />
+            KVK: 91034396
+            <br />
+            BTW: NL004861037B68
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <ul className="w-1/2 text-right">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/tyz81/"
+                target="_blank"
+                rel="noreferrer"
+                title="Download my Curriculum Vitae"
+                download
+              >
+                Curriculum Vitae
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/tyz81/"
+                target="_blank"
+                rel="noreferrer"
+                title="View my profile on LinkedIn"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/tyz81/"
+                target="_blank"
+                rel="noreferrer"
+                title="View my profile on Github"
+              >
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://codepen.io/_TyZ_"
+                target="_blank"
+                rel="noreferrer"
+                title="View my profile on Github"
+              >
+                Codepen
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/tyz81/"
+                target="_blank"
+                rel="noreferrer"
+                title="Download my Terms and Conditions"
+                download
+              >
+                Terms and Conditions
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </main>
-  )
+  );
 }
