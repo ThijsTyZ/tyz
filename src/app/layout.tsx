4617d2622d09dd56a2379f46bb2c226cdf8483a1
Thijs <thijs@tyz.nl>
2023-08-29 11:54:30 +0200

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,26 @@ import type React from 'react';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'TyZ - Interim Tech Lead',
+  title:
+    'TyZ - Thijs Broerse - Interim Tech Lead & Freelance Frontend Architect',
   description:
     'Highly skilled Frontend expert with 20+ years of experience in web development and leadership. Able to set up, develop and maintain complex (frontend) systems, leading (international) development teams. Worked on many (award winning) projects specialized in scripting and architecture.',
+  keywords: [
+    'Frontend',
+    'React',
+    'TypeScript',
+    'Thijs Broerse',
+    'TyZ',
+    'Freelance',
+    'interim Frontend Lead',
+    'Interim Tech Lead',
+    'Angular',
+    'Vue',
+    'Architecture',
+    'Developer',
+    'Development',
+    'Website',
+  ],
 };
 
 export default function RootLayout({
