e8791601552b9f917d3f57c72f48646ecef53d58
Thijs <thijs@tyz.nl>
2023-08-29 10:31:39 +0200

    Update alignment Add CV remove old SVG files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,79 @@
 export default function Home(): JSX.Element {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <div className="w-full flex flex-col items-center min-h-screen justify-between">
       <header className="w-full flex flex-col items-center p-10">
         <div className="flex max-w-5xl w-full">
           <div className="w-1/2">
-            <h1 className="text-stone-800 text-5xl">TyZ</h1>
-            <h3 className="text-stone-700 text-xl">Think. Yield. Zenith.</h3>
+            <h1 className="text-stone-800 text-4xl">TyZ</h1>
+            <h3 className="text-stone-700 text-s">Think. Yield. Zenith.</h3>
           </div>
-
-          <p className="w-1/2 text-right">
+          <p className="w-1/2 text-right text-xl">
             <a href="mailto:thijs@tyz.nl">thijs@tyz.nl</a>
             <br />
-            <a href="tel:0689936821">06 899 36 821</a>
+            <a href="tel:+31689936821">+31 (0) 6 899 36 821</a>
           </p>
         </div>
       </header>
 
-      <div className="max-w-5xl relative flex flex-col place-items-center">
-        <h2 className="text-stone-800 text-7xl p-5">Thijs Broerse</h2>
-        <h3 className="text-stone-700 text-3xl">
-          Interim Tech Lead & Freelance Frontend Architect
-        </h3>
+      <main className="w-full flex flex-col  items-center p-10">
+        <div className="max-w-5xl relative flex flex-col lg:place-items-center">
+          <h2 className="text-black text-7xl py-5">Thijs Broerse</h2>
+          <h3 className="text-stone-700 text-3xl max-w-5xl">
+            Interim Tech Lead & Freelance Frontend Architect
+          </h3>
 
-        <p className="p-10">
-          Highly skilled Frontend expert with over 20 years of experience in web
-          development and leadership. Able to set up, develop and maintain
-          complex (frontend) systems, leading (international) development teams.
-          Worked on many (award winning) projects, specialized in scripting and
-          architecture
-        </p>
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
-        <div className="">
-          <h2 className="mb-3 text-2xl font-semibold">Leadership</h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Lead and mentor development teams with agile methodologies
+          <p className="py-5 lg:text-justify lg:max-w-3xl">
+            Highly skilled Frontend expert with over 20 years of experience in
+            web development and leadership. Able to set up, develop and maintain
+            complex (frontend) systems, leading (international) development
+            teams. Worked on many (award winning) projects, specialized in
+            scripting and architecture
           </p>
         </div>
 
-        <div className="">
-          <h2 className="mb-3 text-2xl font-semibold">Architecture</h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Build complex web applications. Setup APIs.
-          </p>
-        </div>
+        <div className="mb-32 grid justify-between lg:max-w-5xl w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+          <div className="py-5 lg:max-w-[30ch]">
+            <h2 className="mb-3 text-2xl font-semibold">Leadership</h2>
+            <p className="m-0 text-sm opacity-50">
+              Unlock your team&amp;apos;s full potential with my leadership. As
+              an experienced leader for development teams, I offer a distinctive
+              approach that blends <strong>mentoring</strong> and{' '}
+              <strong>Agile</strong> methodologies seamlessly. With a focus on{' '}
+              <strong>personal growth</strong> within an international context,
+              I don&amp;apos;t just drive projects forward – I cultivate
+              individuals and teams, and elevate your team&amp;apos;s
+              performance to new heights.
+            </p>
+          </div>
 
-        <div className="">
-          <h2 className="mb-3 text-2xl font-semibold">Development</h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Write solid and clean code with TypeScript or plain JS. Preferred in
-            React or Angular, Vue or Vanilla JS
-          </p>
+          <div className="py-5 lg:max-w-[30ch]">
+            <h2 className="mb-3 text-2xl font-semibold">Architecture</h2>
+            <p className="m-0 text-sm opacity-50">
+              Building for Change with Solid APIs. In the realm of architecture,
+              I specialize in crafting robust solutions for intricate
+              challenges, ensuring your systems remain agile and adaptive
+              through smart <strong>API</strong> integration that creates a
+              foundation embracing change. Let&amp;apos;s collaborate to conquer
+              complexity and construct a future-ready framework.
+            </p>
+          </div>
+
+          <div className="py-5 lg:max-w-[30ch] ">
+            <h2 className="mb-3 text-2xl font-semibold">Development</h2>
+            <p className="m-0 text-sm opacity-50">
+              In the world of development, I bring high standards, robustness,
+              and a performance-driven approach to the table. With expertise in{' '}
+              <strong>TypeScript</strong>, I create inclusive experiences
+              through <strong>Accessibility</strong>
+              (A11Y) integration. While my preference is <strong>React</strong>,
+              I&amp;apos;m proficient in <strong>Angular</strong> and{' '}
+              <strong>Vue</strong> as well. Let&amp;apos;s collaborate to
+              deliver top-tier solutions that redefine possibilities and leave a
+              lasting mark.
+            </p>
+          </div>
         </div>
-      </div>
+      </main>
       <footer className="w-full text-white flex flex-col items-center p-10">
         <div className="flex max-w-5xl w-full">
           <p className="w-1/2">
@@ -70,7 +90,7 @@ export default function Home(): JSX.Element {
           <ul className="w-1/2 text-right">
             <li>
               <a
-                href="https://www.linkedin.com/in/tyz81/"
+                href="./Curriculum Vitae - Thijs Broerse.pdf"
                 target="_blank"
                 rel="noreferrer"
                 title="Download my Curriculum Vitae"
@@ -123,6 +143,6 @@ export default function Home(): JSX.Element {
           </ul>
         </div>
       </footer>
-    </main>
+    </div>
   );
 }
